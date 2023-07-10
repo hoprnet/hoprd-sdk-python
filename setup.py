a841265ca19c838d8d9976b74bd54ecd7afb1ae5
ausias-armesto <ausiasarmesto@gmail.com>
2023-07-10 15:58:13 +0200

    Renaming Python SDK

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 
 from setuptools import setup, find_packages  # noqa: H301
 
-NAME = "hoprd-api-client"
+NAME = "hoprd-sdk-python"
 VERSION = "1.0.0"
 # To install the library, run the following
 #
