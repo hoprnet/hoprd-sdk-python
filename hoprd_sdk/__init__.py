547a933b446a935e4a86e188aabefdfa2da052c7
Tibor Csóka <csokat@gmail.com>
2024-03-26 22:34:11 +0100

    Update configuration and ticket statistics endpoints

--- hoprd_sdk/__init__.py
+++ hoprd_sdk/__init__.py
@@ -19,6 +19,7 @@ from hoprd_sdk.api.account_api import AccountApi
 from hoprd_sdk.api.alias_api import AliasApi
 from hoprd_sdk.api.channels_api import ChannelsApi
 from hoprd_sdk.api.checks_api import ChecksApi
+from hoprd_sdk.api.configuration_api import ConfigurationApi
 from hoprd_sdk.api.messages_api import MessagesApi
 from hoprd_sdk.api.network_api import NetworkApi
 from hoprd_sdk.api.node_api import NodeApi
